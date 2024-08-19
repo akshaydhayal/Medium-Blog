1e27d558f7cc1701b5e18a7f717fdb0ae874bb4d
akshaydhayal <akshaydhayalkd99@gmail.com>
2024-08-19 20:49:33 +0530

    Added cache to store loaded blogs and Added specific BlogPage

--- frontend/src/hooks/useGetAllBlogs.ts
+++ frontend/src/hooks/useGetAllBlogs.ts
@@ -1,14 +1,18 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { addBlogs } from "../store/blogCache";
 
 export default function useGetAllBlogs(){
     const [blogs,setBlogs]=useState();
+    const dispatch=useDispatch();
     useEffect(()=>{
         async function getAllBlogs(){
             const response=await axios.get(`${import.meta.env.VITE_BACKEND_API}/blogs/all`);
             console.log(response.data);
             if(response.data){
                 setBlogs(response.data);
+                dispatch(addBlogs(response.data));
             }
         }
         getAllBlogs();
